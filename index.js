fa2aae39714a81c571cabc04a11109dc5aae6c91
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-29 19:23:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,5 +51,5 @@ function allWagesFor(record) {
 }
 
 function calculatePayroll(records) {
-  return records.reduce((total, employee) => total += allWagesFor(employee))
+  return records.reduce((total, employee) => total + allWagesFor(employee), 0)
 }
