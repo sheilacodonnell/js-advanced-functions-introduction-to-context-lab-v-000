a9d94d80630b8ba79fcecded5a98fe24f78ca07a
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-29 18:39:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function createEmployeeRecord(employee) {
-  return firstName = employee[0]
+function createEmployeeRecord(record) {
+  return {
+    firstName: record[0],
+    familyName: record[1],
+    title: record[2],
+    payPerHour: record[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
 }
